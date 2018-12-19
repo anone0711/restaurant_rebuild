293b8dc5c3db0a9f4f0762f140844cdacccb4a8c
Brian <finder.chen@gmail.com>
2018-12-19 21:22:01 +0800

    setup comment resources for public users

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   validates_presence_of :name
   mount_uploader :image, PhotoUploader
+  has_many :comments, dependent: :destroy
   belongs_to :category
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :comments, dependent: :destroy
   def admin?
     self.role == "admin"
   end
-  
+
 end
